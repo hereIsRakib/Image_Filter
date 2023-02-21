26039240ba7105992b6bb0303fe823a10ae35b94
hereIsRakib <hossain.rakib8011@gmail.com>
2023-02-22 03:29:22 +0600

    modified commit

--- ImgFilter.py
+++ ImgFilter.py
@@ -1,25 +1,57 @@
 from PIL import ImageFilter
 
 
-def filter_img(img):
-    param = int(input(
-        'Please Enter a Following Number to Filter the Photo.\nFor Blur Enter: 1\nFor Contour Enter: 2\nFor Detail Enter: 3\nFor Edge Enhance Enter: 4\nFor More Enhance Enter: 5\nFor EMBOSS Enter: 6\nFor Find Edges Enter: 7\nFor Sharpen Enter: 8\nFor Smooth Enter: 9\n'))
+def select_filter():
+    valid_options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    while True:
+        param = input('Please Enter a Number to Select a Filter:\n'
+                      '1. Blur\n'
+                      '2. Contour\n'
+                      '3. Detail\n'
+                      '4. Edge Enhance\n'
+                      '5. More Enhance\n'
+                      '6. Emboss\n'
+                      '7. Find Edges\n'
+                      '8. Sharpen\n'
+                      '9. Smooth\n')
+        try:
+            param = int(param)
+            if param in valid_options:
+                return param
+            else:
+                print('Invalid option. Please enter a number from 1 to 9.')
+        except ValueError:
+            print('Invalid input. Please enter a number from 1 to 9.')
+
+
+def apply_filter(img, param):
     if param == 1:
-        img1 = img.filter(ImageFilter.BLUR)
+        filtered_img = img.filter(ImageFilter.BLUR)
     elif param == 2:
-        img1 = img.filter(ImageFilter.CONTOUR)
+        filtered_img = img.filter(ImageFilter.CONTOUR)
     elif param == 3:
-        img1 = img.filter(ImageFilter.DETAIL)
+        filtered_img = img.filter(ImageFilter.DETAIL)
     elif param == 4:
-        img1 = img.filter(ImageFilter.EDGE_ENHANCE)
+        filtered_img = img.filter(ImageFilter.EDGE_ENHANCE)
     elif param == 5:
-        img1 = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+        filtered_img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
     elif param == 6:
-        img1 = img.filter(ImageFilter.EMBOSS)
+        filtered_img = img.filter(ImageFilter.EMBOSS)
     elif param == 7:
-        img1 = img.filter(ImageFilter.FIND_EDGES)
+        filtered_img = img.filter(ImageFilter.FIND_EDGES)
     elif param == 8:
-        img1 = img.filter(ImageFilter.SHARPEN)
+        filtered_img = img.filter(ImageFilter.SHARPEN)
     elif param == 9:
-        img1 = img.filter(ImageFilter.SMOOTH)
-    return img1.save('converted_image.png', 'png')
+        filtered_img = img.filter(ImageFilter.SMOOTH)
+    return filtered_img
+
+
+def save_image(img, filename):
+    img.save(filename, 'PNG')
+    print(f'Filtered image saved as {filename}')
+
+
+def filter_img(img):
+    param = select_filter()
+    filtered_img = apply_filter(img, param)
+    save_image(filtered_img, 'converted_image.png')
